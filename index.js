0080e0b988d83b5ba322622b7c09356e0e96b191
Augusto Martinez <augustomartinezt5@gmail.com>
2021-08-17 19:12:55 -0300

    Creacion de la seccion de consultas y agregado de modals

---
+++ index.js
@@ -0,0 +1,13 @@
+let modal_fondo = document.getElementById("modal-fondo");
+let modal = document.getElementById("modal");
+let btn = document.getElementById("submit");
+
+btn.addEventListener("click",()=>{
+    modal.classList.add("animacion-modal");
+    modal_fondo.classList.remove("modal-ocultar");
+    setTimeout(()=>{
+        modal_fondo.classList.add("modal-ocultar");
+        modal.classList.remove("animacion-modal")
+    },2500);
+
+});
