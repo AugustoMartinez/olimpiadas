32aedca4306d3f8ffeb84f853418887cf20907a8
Augusto Martinez <augustomartinezt5@gmail.com>
2021-08-17 20:50:57 -0300

    No se me ocurrio nada que poner

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 let modal_fondo = document.getElementById("modal-fondo");
 let modal = document.getElementById("modal");
 let btn = document.getElementById("submit");
+let input_eliminar = document.getElementById("codigo-eliminar");
+
 
 btn.addEventListener("click",()=>{
     modal.classList.add("animacion-modal");
